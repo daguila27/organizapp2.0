b002e3d66d1474a0ad11f0b4d9a6c16064de8f58
daguila27 <daguila.gallardo@gmail.com>
2016-09-12 04:13:37 -0300

    se agrego un layout con un listview con las categorias(certamen, aniversario, reunion, etc)

--- app/src/main/java/com/example/daniel/beta2/MainActivity.java
+++ app/src/main/java/com/example/daniel/beta2/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.daniel.beta2;
 
-import android.database.CharArrayBuffer;
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.CalendarView;
-import android.widget.Spinner;
-import android.widget.TextView;
+        import android.content.Context;
+        import android.database.CharArrayBuffer;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.KeyEvent;
+        import android.view.View;
+        import android.widget.ArrayAdapter;
+        import android.widget.CalendarView;
+        import android.widget.ListView;
+        import android.widget.Spinner;
+        import android.widget.TextView;
 
-import java.util.Stack;
-import org.w3c.dom.Text;
+        import java.lang.reflect.Array;
+        import java.util.Stack;
+        import org.w3c.dom.Text;
+
+        import static com.example.daniel.beta2.CategoriasTabla.CategoriasEntry;
+        import static com.example.daniel.beta2.NotificacionesTabla.NotificacionesEntry;
+        import static com.example.daniel.beta2.EventosTabla.EventosEntry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +29,20 @@ public class MainActivity extends AppCompatActivity {
     private Stack contentViewsPila;
     private int selectedYear, selectedMonth, selectedDay;
 
+    private SQLiteDatabase base_categorias;
+    private SQLiteDatabase base_notificaciones;
+    private SQLiteDatabase base_eventos;
+
+    Context estado;//informacion respecto al estado del entorno de la aplicacion...
+    CategoriasHelper categorias = new CategoriasHelper(estado);
+    NotificacionesHelper notificaciones = new NotificacionesHelper(estado);
+    EventosHelper eventos = new EventosHelper(estado);
+
     public void setCurrentViewById(int id){
         setContentView(id);
         currentViewId = id;
         contentViewsPila.push(id);
-        
+
         crearActivity();
     }
 
@@ -51,8 +68,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState); //Por defecto
 
-        contentViewsPila = new Stack(); //Para saber los contents views que se han ido cambiando
+        categorias.onCreate(base_categorias);//SE INICIAN LAS BASES DE DATOS
+        notificaciones.onCreate(base_notificaciones);
+        eventos.onCreate(base_eventos);
 
+        contentViewsPila = new Stack(); //Para saber los contents views que se han ido cambiando
+        crear_categorias_config();
         setCurrentViewById(R.layout.activity_main); //Pone la actividaad principal en pantalla
         setearOnClickCalendario(R.id.calendarView); //Para almacenar la fecha que el usuario seleccione
 
@@ -125,17 +146,37 @@ public class MainActivity extends AppCompatActivity {
         TextView fecha = (TextView) findViewById(R.id.crearNotificacionFecha);
         fecha.append(date);
     }
-/*
-    public CharArrayBuffer[] list_database(Cursor database){
 
-        CharArrayBuffer[] list = new CharArrayBuffer[database.getColumnCount()];
-        database.moveToFirst();
-        do{
-            CharArrayBuffer buffer = new CharArrayBuffer();
-            database.copyStringToBuffer(database.getPosition(),buffer);
-            list.append(buffer);
-        }while(!database.isLast());
-    return list;
+    public String[][] getcategorias(Cursor categoria , boolean tipe){
+        categoria.moveToFirst();//SE MUEVE EL CURSOR A LA PRIMERA FILA YA QUE ESTE COMIENZA EN LA FILA -1
+        String[][] matrix = new String[categoria.getColumnCount()-1][categoria.getCount()];
+        //getCount() retorna el numero de filas que existen
+        //getColumnCount() retorna el numero de columnas, se le resta una porque no queremos contar la columna con el id de la categoria
+        int prioridad_columna = categoria.getColumnIndex(CategoriasEntry.PRIORIDAD);
+        int nombre_columna = categoria.getColumnIndex(CategoriasEntry.NOMBRE);
+        categoria.moveToFirst();//PORSIACASO...
+        while(categoria.moveToNext()){
+            if(tipe) {//true = string[columna][fila]
+                matrix[prioridad_columna][categoria.getPosition()] = categoria.getString(prioridad_columna);
+                //getPosicion() retorna la fila en la que se encuentra el cursor
+                matrix[nombre_columna][categoria.getPosition()] = categoria.getString(nombre_columna);
+            }
+            else{//false = string[fila][columna]
+                matrix[categoria.getPosition()][prioridad_columna] = categoria.getString(prioridad_columna);
+                //getPosicion() retorna la fila en la que se encuentra el cursor
+                matrix[categoria.getPosition()][nombre_columna] = categoria.getString(nombre_columna);
+            }
+        }
+        return matrix;
     }
-    */
+
+
+    public void crear_categorias_config(){
+        Cursor pointer = categorias.getAllCategorias();
+        String[][] matrix = getcategorias(categorias.getAllCategorias(),false);
+        String[] categorias_array = matrix[1];
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, R.layout.categorias_list_item, categorias_array);
+        ListView lv =(ListView)findViewById(R.id.categorias_config);
+        lv.setAdapter(adaptador);
+    }
 }
